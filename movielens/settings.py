1ae9e13ee086c78dd2dc9acf58c98411acaf7e78
Jan Cofta <jan.cofta@gmail.com>
2019-06-21 16:15:05 +0200

    Disable debug mode

--- movielens/settings.py
+++ movielens/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(p@(=wr9$6bjp0y7n^a#s2y5oxlz#e==tzz)n&y2oegdv^y7*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'jcofta.pythonanywhere.com',
